dfef9627cce01f1afacb5e26b05700db39f46781
simonerom <mail@sromano.com>
2022-11-06 19:12:31 +0100

    Added comments to the Applet source code

--- w3bstream/src/click2earn.go
+++ w3bstream/src/click2earn.go
@@ -1,45 +1,48 @@
+// W3bstream logic to handle the device message
 package main
 
 import (
 	"fmt"
 
-	"github.com/tidwall/gjson"
-
 	common "github.com/machinefi/w3bstream/_examples/wasm_common_go"
+	"github.com/tidwall/gjson"
 )
 
 func main() {}
 
-//export start
-func _start(rid uint32) int32 {
-	common.Log(fmt.Sprintf("start received: %d", rid))
-	message, err := common.GetDataByRID(rid)
+// Exported function to serve as a W3bstream handler
+func _start(event_id uint32) int32 {
+	common.Log(fmt.Sprintf("start handler called with event id: %d", event_id))
+	// Get the payload attached to the W3bstream event message
+	payload, err := common.GetDataByRID(event_id)
 	if err != nil {
-		common.Log("error: " + err.Error())
+		common.Log("Error while getting event data: " + err.Error())
 		return -1
 	}
-	res := string(message)
+	res := string(payload)
 
+	// Get the recipient address from the payload
 	Account := gjson.Get(res, "Account")
-	common.Log(fmt.Sprintf("Account: %s", Account.String()))
+	// Get the current number of clicks from the key-value store
 	count := common.GetDB("clicks") + 1
-
-	common.Log(fmt.Sprintf("number of clicks: %d", count))
-
+	common.Log(fmt.Sprintf("Current number of clicks: %d", count))
+	// Update the number of clicks in the key-value store
 	common.SetDB("clicks", count)
-
+	// Send the reward to the recipient address every 5 clicks
 	if count%5 == 0 {
-		common.Log("sending tx....")
+		common.Log("Sending rewards transaction....")
 		common.SendTx(fmt.Sprintf(
 			`{
 				"to": "%s",
 				"value": "0",
 				"data": "40c10f19000000000000000000000000%s0000000000000000000000000000000000000000000000000de0b6b3a7640000"
 			}`,
-			"0x3Fb8306Ca5C7d5A752b195F1df2f03CCE6BC1Bcb", //W3BERC20 contract address
-			Account.String(), //send to account
+			// Mintable token contract address
+			"0x3Fb8306Ca5C7d5A752b195F1df2f03CCE6BC1Bcb",
+			// Recipient account, indicated in the device message
+			Account.String(),
 		))
-		common.Log("tx sent successfully")
+		common.Log("Transaction sent.")
 	}
 
 	return 0
